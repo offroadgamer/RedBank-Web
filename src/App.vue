518113eae9157fc95e6ed06e4abfac49a18c18eb
Ola Kristensen <olafkri05@gmail.com>
2026-01-02 22:55:26 +0100

    Created header and started with transactions box

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Header from './components/Header.vue';
 </script>
 
 <template>
- <h1>This is a test</h1>
+  <Header></Header>
+  <div class="main">
+    <div class="welcome-message-container">
+      <h1>Welcome to RedBank!</h1>
+      <div class="trainsactions-container">
+        <div class="trainsactions-balance">
+          <h3>Your balance: $0</h3>
+        </div>
+        <div class="trainsactions-button-container">
+          <button class="btn-big">
+            <h3>Deposit</h3>
+          </button>
+          <button class="btn-big">
+            <h3>Withdraw</h3>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .welcome-message-container {
+    margin-top: 50px;
+  }
+  .trainsactions-container {
+    text-align: center;
+    margin-top: 40px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .trainsactions-button-container {
+    margin-top: 20px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<script>
+	
+</script>
+
+<template>
+	<div class="navbar-container">
+		<div class="logo-container">
+			<img src="@/assets/redbank_logo.jpg" alt="website logo">
+		</div>
+		<div class="title-container">
+			<h2>RedBank</h2>
+		</div>
+		<div class="menu-container">
+			<div class="burger-menu-bar"></div>
+			<div class="burger-menu-bar"></div>
+			<div class="burger-menu-bar"></div>						
+		</div>
+	</div>
+</template>
+
+<style>
+	template {
+		margin: 0px;
+	}
+
+	img {
+		height: 5rem;
+	}
+
+	.navbar-container {
+		width: 100%;
+		height: 8rem;
+		box-shadow: 0px 20px 20px #aaaaaa;
+		display: flex;
+		align-items: center;
+	}
+
+	.logo-container {
+		width: 5%;
+		display: flex;
+		justify-content: right;
+		padding: 5px;
+	}
+
+	.title-container {
+		width: 85%;
+	}
+	
+	.menu-container {
+		width: 10%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.burger-menu-bar {
+		height: 7px;
+		width: 50px;
+		background-color: black;
+		margin: 4px;
+	}
+
+</style>
