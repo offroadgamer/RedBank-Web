dc3f9e4a8cecb8a8064505aeef8876d2b09f726b
Ola Kristensen <olafkri05@gmail.com>
2026-03-25 00:33:49 +0100

    Added all login messages and errors as GUI

--- src/components/LoginMenu.vue
+++ src/components/LoginMenu.vue
@@ -10,8 +10,10 @@
 
   const usernameError = ref(false);
   const passwordError = ref(false);
+  const loginStatus = ref(false);
   const usernameErrorMessage = ref("");
   const passwordErrorMessage = ref("");
+  const loginStatusMessage = ref("");
 
   function handleLoginError(type, message) {
     if(type == "username") {
@@ -22,11 +24,16 @@
       passwordError.value = true;
       passwordErrorMessage.value = message;
     }
+    if(type == "loginStatus") {
+      loginStatus.value = true;
+      loginStatusMessage.value = message;
+    }
   }
 
   function handleAuth() {
     usernameError.value = false;
     passwordError.value = false;
+    loginStatus.value = false;
     if(usernameField.value.replace(/\s+/g, "") == "") {
       console.log("Username must be filled out!")
       handleLoginError("username", "This field must be filled out!")
@@ -44,12 +51,15 @@
       const usernameItem = userData.items.find(item => item.username == usernameField.value)
       if(usernameItem == undefined) {
         console.log("E-mail does not exist in database")
+        handleLoginError("username", "E-mail does not exist in database")
       } else {
         const passwordItem = userData.items.find(item => item.password == passwordField.value)
         if(passwordItem == undefined || userData.items[usernameItem.id] != userData.items[passwordItem.id]) {
           console.log("Wrong password!")
+          handleLoginError("password", "Wrong password!")
         } else {
           console.log("Successfully logged in!")
+          handleLoginError("loginStatus", "Successfully logged in!")
         }
       }
     }
@@ -68,6 +78,7 @@
     <h4>Password:</h4>
     <input v-model="passwordField">
     <h5 v-if="passwordError" class="login-error">{{passwordErrorMessage}}</h5>
+    <h5 v-if="loginStatus" class="login-status">{{loginStatusMessage}}</h5>
     <button class="btn-small" id="login-btn" @click="handleAuth()">
       <h5 v-if="signup">Sign up</h5>
       <h5 v-else>Login</h5>
@@ -91,6 +102,7 @@
     color: var(--main-color);
   }
 
-  
-  
+  .login-status {
+    color: var(--green);
+  }
 </style>
